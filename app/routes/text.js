8c8ddbe282e579e2039d436cab8f0fdfd21d8830
go-ka <go_@live.jp>
2021-04-07 00:04:32 -0500

    Comment logs

--- app/routes/text.js
+++ app/routes/text.js
@@ -11,7 +11,7 @@ export default class TextRoute extends Route {
   async getItems(itemType = 'gifs') {
     //type: gifs / stickers / text
     const tGifs = await gf.trending({ type: itemType, limit: 25, offset: 0, rating: 'g' });
-    console.log(tGifs);
+    // console.log(tGifs);
     return tGifs.data;
   }
 }
