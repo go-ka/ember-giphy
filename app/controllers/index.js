66d9eacdaf3a2a349bd3f65f116a1479181fff3f
go-ka <go_@live.jp>
2021-04-16 14:24:46 -0500

    Load More Function

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,32 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { GiphyFetch } from '@giphy/js-fetch-api';
+import ENV from 'ember-giphy/config/environment';
+
+const gf = new GiphyFetch(ENV.GIPHY_API_KEY);
 
 export default class IndexController extends Controller {
   @tracked gifsFilterValue;
   // model
   @tracked filteredGifs = this.model;
-  
+  offset = 1;
+
   @action filterGifs() {
-    this.filteredGifs = this.model.filter(item => item.title.toLowerCase().includes(this.gifsFilterValue.toLowerCase()));
+    this.filteredGifs = this.model.filter((item) =>
+      item.title.toLowerCase().includes(this.gifsFilterValue.toLowerCase())
+    );
+  }
+
+  @action async loadMoreItems(itemType = 'gifs') {
+    const gifs = await gf.trending({
+      type: itemType,
+      limit: 25,
+      offset: 25 * this.offset,
+      rating: 'g',
+    });
+    this.model.pushObjects(gifs.data);
+    this.filterGifs();
+    this.offset++;
   }
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,20 @@
 import Route from '@ember/routing/route';
-import { GiphyFetch } from '@giphy/js-fetch-api'
+import { GiphyFetch } from '@giphy/js-fetch-api';
 import ENV from 'ember-giphy/config/environment';
-
 const gf = new GiphyFetch(ENV.GIPHY_API_KEY);
-
 export default class IndexRoute extends Route {
-
   async model() {
     return this.getItems('gifs');
   }
   async getItems(itemType = 'gifs') {
     //type: gifs / stickers / text
-    const tGifs = await gf.trending({ type: itemType, limit: 25, offset: 0, rating: 'g' });
+    const tGifs = await gf.trending({
+      type: itemType,
+      limit: 25,
+      offset: 0,
+      rating: 'g',
+    });
     // console.log(tGifs);
     return tGifs.data;
   }
-  
 }
